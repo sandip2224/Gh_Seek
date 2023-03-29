6d12913a7969531268d8ffed7973e416117ec4ad
sandip2224 <sandipan2224@gmail.com>
2023-03-29 22:04:02 +0530

    Basic setup + design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import './App.css';
+import './index.css'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import NotFound from './components/pages/NotFound'
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <Router>
+      <div className='App flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+
   );
 }
 
