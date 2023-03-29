7aac709d72ac207cdf0cc3533f29656b83dfe91e
sandip2224 <sandipan2224@gmail.com>
2023-03-29 22:05:01 +0530

    Common pages Home, About, NotFound setup

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,7 @@
+const About = () => (
+  <>
+    <h1 className='text-6xl mb-4'>Gh | Seeker</h1>
+  </>
+);
+
+export default About;
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => (
+  <>
+    <h1 className='text-6xl'>Welcome</h1>
+  </>
+);
+
+export default Home;
