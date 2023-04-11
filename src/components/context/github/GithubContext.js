4e11cbcf7f8266a35b8bb1eb68ec7ef2357b8687
sandip2224 <sandipan2224@gmail.com>
2023-04-11 21:55:50 +0530

    useReducer hook use

--- src/components/context/github/GithubContext.js
+++ src/components/context/github/GithubContext.js
@@ -9,12 +9,13 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true
+    loading: false
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
   const fetchUsers = async () => {
+    setLoading()
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`
@@ -25,6 +26,13 @@ export const GithubProvider = ({ children }) => {
     dispatch({
       type: 'GET_USERS',
       payload: data
+    })
+  }
+
+  // Set loading
+  const setLoading = () => {
+    dispatch({
+      type: 'SET_LOADING'
     })
   }
 
