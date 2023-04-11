464bbaa542b901b59130f3bc41e366aa2df3dc36
sandip2224 <sandipan2224@gmail.com>
2023-04-11 19:38:23 +0530

    Migrating to context + refactoring

--- src/App.js
+++ src/App.js
@@ -9,23 +9,28 @@ import Home from './components/pages/Home'
 import About from './components/pages/About'
 import NotFound from './components/pages/NotFound'
 
+import { GithubProvider } from './components/context/github/GithubContext';
+
 function App() {
   return (
-    <Router>
-      <div className='App flex flex-col justify-between h-screen'>
-        <Navbar />
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/notfound' element={<NotFound />} />
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-
+    <GithubProvider>
+      <>
+        <Router>
+          <div className='App flex flex-col justify-between h-screen'>
+            <Navbar />
+            <main className='container mx-auto px-3 pb-12'>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </>
+    </GithubProvider>
   );
 }
 
