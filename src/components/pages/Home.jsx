d81bc694327e33efdcb38ce3acb51b8aa4e495dc
sandip2224 <sandipan2224@gmail.com>
2023-04-14 13:11:43 +0530

    Add userSearch component in HomePage

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,10 @@
 import UserResults from '../users/UserResults';
+import UserSearch from '../users/UserSearch';
 
 const Home = () => {
   return (
     <>
+      <UserSearch />
       <UserResults />
     </>
   )
