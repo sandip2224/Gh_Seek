4653d65fecfb8af654bb5655c403a7456bcdda95
sandip2224 <sandipan2224@gmail.com>
2023-04-14 13:10:49 +0530

    GithubContext searchUsers

--- src/components/context/github/GithubContext.js
+++ src/components/context/github/GithubContext.js
@@ -14,18 +14,24 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
-  const fetchUsers = async () => {
+  // Get search results
+  const searchUsers = async (text) => {
     setLoading()
-    const response = await fetch(`${GITHUB_URL}/users`, {
+
+    const params = new URLSearchParams({
+      q: text
+    })
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`
       }
     })
 
-    const data = await response.json()
+    const { items } = await response.json()
     dispatch({
       type: 'GET_USERS',
-      payload: data
+      payload: items
     })
   }
 
@@ -36,10 +42,17 @@ export const GithubProvider = ({ children }) => {
     })
   }
 
+  const clearUsers = () => {
+    dispatch({
+      type: 'CLEAR_USERS'
+    })
+  }
+
   return <GithubContext.Provider value={{
     users: state.users,
     loading: state.loading,
-    fetchUsers
+    searchUsers,
+    clearUsers
   }}>
     {children}
   </GithubContext.Provider>
