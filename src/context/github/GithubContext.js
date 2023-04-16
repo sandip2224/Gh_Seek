f96051f4d3d6b254b0e1914624f1fdbd3ddae911
sandip2224 <sandipan2224@gmail.com>
2023-04-16 11:13:13 +0530

    Added toastmessages + getRepos

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,6 +1,8 @@
 import { createContext, useReducer } from 'react';
 import githubReducer from './GithubReducer';
 
+import { successToast, infoToast, errorToast } from '../../components/error/errorHandler'
+
 const GithubContext = createContext()
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
@@ -10,6 +12,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false
   }
 
@@ -23,54 +26,71 @@ export const GithubProvider = ({ children }) => {
       q: text
     })
 
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`
-      }
-    })
+    try {
+      const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`
+        }
+      })
 
-    const { items } = await response.json()
-    dispatch({
-      type: 'GET_USERS',
-      payload: items
-    })
+      const { items } = await response.json()
+      successToast("Users fetched successfully!")
+      dispatch({
+        type: 'GET_USERS',
+        payload: items
+      })
+    }
+    catch (err) {
+      errorToast(err)
+    }
   }
 
   // Get singleUser
   const getUser = async (username) => {
     setLoading()
 
-    const response = await fetch(`${GITHUB_URL}/users/${username}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`
-      }
-    })
-
-    if (response.status === 404) {
-      window.location = '/notfound'
-    }
-    else {
-      const data = await response.json()
-      dispatch({
-        type: 'GET_USER',
-        payload: data
+    try {
+      const response = await fetch(`${GITHUB_URL}/users/${username}`, {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`
+        }
       })
+
+      if (response.status === 404) {
+        window.location = '/notfound'
+        infoToast('User not found!')
+      }
+      else {
+        const data = await response.json()
+        successToast('User fetched successfully!')
+        dispatch({
+          type: 'GET_USER',
+          payload: data
+        })
+      }
+    }
+    catch (err) {
+      errorToast(err)
     }
   }
 
   const getUserRepos = async (username) => {
     setLoading()
 
-    const res = await axios.get(
-      `${GITHUB_URL}/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
-    );
+    try {
+      const response = await fetch(`${GITHUB_URL}/users/${username}/repos?per_page=5&sort=created:asc`)
+      const data = await response.json()
 
-    dispatch({
-      type: GET_REPOS,
-      payload: res.data
-    });
+      successToast('Repositories fetched successfully!')
+      dispatch({
+        type: 'GET_REPOS',
+        payload: data
+      });
+    }
+    catch (err) {
+      errorToast(err)
+    }
   };
-
 
   // Set loading
   const setLoading = () => {
@@ -80,6 +100,7 @@ export const GithubProvider = ({ children }) => {
   }
 
   const clearUsers = () => {
+    infoToast('Cleared all fetched users!')
     dispatch({
       type: 'CLEAR_USERS'
     })
@@ -89,8 +110,10 @@ export const GithubProvider = ({ children }) => {
     users: state.users,
     loading: state.loading,
     user: state.user,
+    repos: state.repos,
     searchUsers,
     getUser,
+    getUserRepos,
     clearUsers
   }}>
     {children}
