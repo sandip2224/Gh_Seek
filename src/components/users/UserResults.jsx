ffc757890e590838fb2032adccc8d048916ce75e
sandip2224 <sandipan2224@gmail.com>
2023-04-16 13:41:32 +0530

    Minor refactoring

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -5,25 +5,25 @@ import UserItem from './UserItem';
 import GithubContext from '../../context/github/GithubContext';
 
 const UserResults = () => {
+	const { users, loading, error } = useContext(GithubContext);
 
-  const { users, loading } = useContext(GithubContext)
-
-  if (!loading) {
-    return (
-      <>
-        <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-          {
-            users.map(user => (
-              <UserItem key={user.id} user={user} />
-            ))
-          }
-        </div>
-      </>
-    );
-  }
-  else {
-    return <Spinner />
-  }
-}
+	if (loading) {
+		return <Spinner />;
+	} else {
+		return (
+			<>
+				<div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+					{users && users.length > 0 ? (
+						users.map((user) => <UserItem key={user.id} user={user} />)
+					) : error ? (
+						<>No users found!</>
+					) : (
+						<></>
+					)}
+				</div>
+			</>
+		);
+	}
+};
 
 export default UserResults;
--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,28 +1,28 @@
-import React, { useState, useContext } from 'react'
-import GithubContext from '../../context/github/GithubContext'
-import AlertContext from '../../context/alert/AlertContext'
+import React, { useState, useContext } from 'react';
+import GithubContext from '../../context/github/GithubContext';
+import AlertContext from '../../context/alert/AlertContext';
 
 function UserSearch() {
-	const [text, setText] = useState('')
+	const [text, setText] = useState('');
 
-	const { users, searchUsers, clearUsers } = useContext(GithubContext)
-	const { setAlert } = useContext(AlertContext)
+	const { users, searchUsers, clearUsers } = useContext(GithubContext);
+	const { setAlert } = useContext(AlertContext);
 
 	const handleChange = (e) => {
-		setText(e.target.value)
-	}
+		setText(e.target.value);
+	};
 
 	const handleSubmit = (e) => {
-		e.preventDefault()
+		e.preventDefault();
 
 		if (text.trim() === '') {
-			setAlert('Please enter something', 'error')
+			setAlert('Please enter something', 'error');
 		} else {
 			// @TODO: Search users
-			searchUsers(text)
-			setText('')
+			searchUsers(text);
+			setText('');
 		}
-	}
+	};
 
 	return (
 		<div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
@@ -44,7 +44,7 @@ function UserSearch() {
 					</div>
 				</form>
 			</div>
-			{users.length > 0 ? (
+			{users && users.length > 0 ? (
 				<div>
 					<div className='btn btn-ghost btn-lg' onClick={clearUsers}>
 						Clear
@@ -54,7 +54,7 @@ function UserSearch() {
 				<></>
 			)}
 		</div>
-	)
+	);
 }
 
-export default UserSearch
+export default UserSearch;
