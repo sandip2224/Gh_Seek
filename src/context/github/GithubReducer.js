fe88d996dcd9df15bfbb39b4640b05c7722deadf
sandip2224 <sandipan2224@gmail.com>
2023-04-16 13:40:37 +0530

    Refactor githubReducer cases

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,27 +1,35 @@
 const githubReducer = (state, action) => {
   switch (action.type) {
-    case 'GET_USERS':
+    case 'SET_USERS':
       return {
         ...state,
         users: action.payload,
-        loading: false
+        loading: false,
+        error: false
       }
-    case 'GET_USER':
+    case 'SET_USER':
       return {
         ...state,
         user: action.payload,
-        loading: false
+        loading: false,
+        error: false
       }
-    case 'GET_REPOS':
-        return {
-          ...state,
-          repos: action.payload,
-          loading: false
-        }
+    case 'SET_REPOS':
+      return {
+        ...state,
+        repos: action.payload,
+        loading: false,
+        error: false
+      }
     case 'SET_LOADING':
       return {
         ...state,
-        loading: true
+        loading: action.payload
+      }
+    case 'SET_ERROR':
+      return {
+        ...state,
+        error: true
       }
     case 'CLEAR_USERS':
       return {
