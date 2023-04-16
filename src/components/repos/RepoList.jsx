0677daa59c02c25e5e3a9887da3bec8ccf111a12
sandip2224 <sandipan2224@gmail.com>
2023-04-16 23:36:47 +0530

    Repository List + single item

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import RepoItem from './RepoItem';
 
 function RepoList({ repos }) {
 	return (
@@ -8,11 +9,11 @@ function RepoList({ repos }) {
 					Latest Respositories
 				</h2>
 				{repos.map((repo) => (
-					<h3>{repo.name}</h3>
+					<RepoItem key={repo.id} repo={repo} />
 				))}
 			</div>
 		</div>
-	)
+	);
 }
 
-export default RepoList
+export default RepoList;
